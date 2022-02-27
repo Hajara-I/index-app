e714bb44bfb53f24f2c8125edd600c2d8ac8e7b0
Hajara <hajaraiyal@gmail.com>
2022-02-27 16:23:51 +0000

    homepage setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { AuthContext } from "./helpers/AuthContext";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const [authState, setAuthState] = useState({
+	// 	username: "",
+	// 	id: 0,
+	// 	status: false,
+	// });
+
+	// useEffect(() => {
+	// 	axios
+	// 		.get("http://localhost:3001/auth/auth", {
+	// 			headers: {
+	// 				accessToken: localStorage.getItem("accessToken"),
+	// 			},
+	// 		})
+	// 		.then((response) => {
+	// 			if (response.data.error) {
+	// 				setAuthState({ ...authState, status: false });
+	// 			} else {
+	// 				setAuthState({
+	// 					username: response.data.username,
+	// 					id: response.data.id,
+	// 					status: true,
+	// 				});
+	// 			}
+	// 		});
+	// }, []);
+
+	return (
+		<div className="App">
+			{/* <AuthContext.Provider value={{ authState, setAuthState }}> */}
+			<Router>
+				<div className="link-container">
+					<Link className="link" to="/">
+						Home
+					</Link>
+					{/* {!authState.status ? (
+							<>
+								<Link className="link" to="/Login">
+									Login
+								</Link>
+								<Link className="link" to="/Registration">
+									Registration
+								</Link>
+							</>
+						) : (
+							<>
+								<button onClick={handleLogout}>Logout</button>
+							</>
+						)}
+						<h1>{authState.username}</h1> */}
+				</div>
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					{/* <Route path="/Registration" element={<Registration />} />
+						<Route path="/Login" element={<Login />} /> */}
+				</Routes>
+			</Router>
+			{/* </AuthContext.Provider> */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Homepage() {
+	return (
+		<div className="home-body" id="home-body">
+			<div className="home-inner">
+				<p>{`Hi `}</p>
+				<p>what are you reading today?</p>
+			</div>
+		</div>
+	);
+}
